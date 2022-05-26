e14b1fd377a87282d1943e69a10d2c946c7f1ce1
krz-max <a1314kevin@gmail.com>
2022-05-26 21:16:15 +0800

    Lab5 double checked

--- Lab5/Lab5Code/Shift_Left_1.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns/1ps
-
-module Shift_Left_1(
-    input  		[32-1:0] data_i,
-    output      [32-1:0] data_o
-    );
-/* Write your code HERE */
-assign data_o = data_i << 1;
-endmodule
